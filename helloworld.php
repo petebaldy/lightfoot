6a357de890b4cb8be29fe0557314cb3941fe8a65
petebaldy <pc130296@gmail.com>
2012-12-28 15:08:12 +0000

    commit helloworld.php

---
+++ helloworld.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title></title>
+<meta name="Generator" content="Editra/0.7.12">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<style type="text/css">
+.default {
+	font-family: Courier New, Trebuchet, Tahoma, sans-serif;
+	font-size: 10;
+	color: #000000;
+	background-color: #FFFFFF;
+}
+</style>
+</head>
+<body class="default">
+<pre>
+
+<?php
+
+echo "hello world from petebaldy!";
+
+?>
+
+</pre>
+</body>
+</html>
